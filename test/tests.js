da1b73580aa4a5759d869ddbd5a5ab95b1a8a3d8
Alex Aaron <alexaaron@Operations-MacBook-Air.local>
2022-10-05 12:29:02 -0500

    add test code for over40()

--- test/tests.js
+++ test/tests.js
@@ -12,4 +12,32 @@ describe("#test", function(){
   it('should run test', function(){
     assert.equal(testData, "test");
   })
-})
+});
+
+describe("#over40", function(){
+  console.log(app.over40.toString());
+  it('should return a number', function(){
+    console.log(app.over40.toString());
+    assert.equal(typeof app.over40(app.patients), 'number');
+  });
+  it('should return the correct number of patients over 40', function(){
+    assert.deepEqual(app.over40(app.patients), 3);
+  });
+  it('should use the reduce method', function(){
+    assert.equal(app.over40.toString().includes('reduce'), true);
+  })
+});
+
+describe("#over40", function(){
+  console.log(app.over40.toString());
+  it('should return a number', function(){
+    console.log(app.over40.toString());
+    assert.equal(typeof app.over40(app.patients), 'number');
+  });
+  it('should return the correct number of patients over 40', function(){
+    assert.deepEqual(app.over40(app.patients), 3);
+  });
+  it('should use the reduce method', function(){
+    assert.equal(app.over40.toString().includes('reduce'), true);
+  })
+});
